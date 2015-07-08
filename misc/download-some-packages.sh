c648b4a42851f02d64a9d2312c6b63cfce801549
Rob Thijssen <rthijssen@gmail.com>
2015-07-08 10:06:55 +0100

    package folder correction

--- misc/download-some-packages.sh
+++ misc/download-some-packages.sh
@@ -1,14 +1,14 @@
-curl -L http://chocolatey.org/api/v2/package/hg/3.4.2 -o packages/hg.3.4.2.nupkg
-curl -L http://chocolatey.org/api/v2/package/hg/3.4.1/ -o packages/hg.3.4.1.nupkg
+curl -L http://chocolatey.org/api/v2/package/hg/3.4.2 -o nupkg/hg.3.4.2.nupkg
+curl -L http://chocolatey.org/api/v2/package/hg/3.4.1 -o nupkg/hg.3.4.1.nupkg
 curl -L http://chocolatey.org/api/v2/package/hg/3.4 -o packages/hg.3.4.nupkg
-curl -L http://chocolatey.org/api/v2/package/hg/2.7.0.20130816/ -o packages/hg.2.7.0.nupkg
+curl -L http://chocolatey.org/api/v2/package/hg/2.7.0.20130816 -o nupkg/hg.2.7.0.nupkg
 
-curl -L http://chocolatey.org/api/v2/package/git.install/1.9.5.20150320 -o packages/git.install.1.9.5.20150320.nupkg
-curl -L http://chocolatey.org/api/v2/package/git.install/1.9.5/ -o packages/git.install.1.9.5.nupkg
-curl -L http://chocolatey.org/api/v2/package/git.install/1.9.0.20140303 -o packages/git.install.1.9.0.20140303.nupkg
-curl -L http://chocolatey.org/api/v2/package/git.install/1.7.10 -o packages/git.install.1.7.10.nupkg
+curl -L http://chocolatey.org/api/v2/package/git.install/1.9.5.20150320 -o nupkg/git.install.1.9.5.20150320.nupkg
+curl -L http://chocolatey.org/api/v2/package/git.install/1.9.5 -o nupkg/git.install.1.9.5.nupkg
+curl -L http://chocolatey.org/api/v2/package/git.install/1.9.0.20140303 -o nupkg/git.install.1.9.0.20140303.nupkg
+curl -L http://chocolatey.org/api/v2/package/git.install/1.7.10 -o nupkg/git.install.1.7.10.nupkg
 
-curl -L http://chocolatey.org/api/v2/package/nxlog/2.8.1248 -o packages/nxlog.2.8.1248.nupkg
-curl -L http://chocolatey.org/api/v2/package/nxlog/2.7.1191 -o packages/nxlog.2.7.1191.nupkg
-curl -L http://chocolatey.org/api/v2/package/nxlog/2.6.1131 -o packages/nxlog.2.6.1131.nupkg
-curl -L http://chocolatey.org/api/v2/package/nxlog/2.5.1089 -o packages/nxlog.2.5.1089nupkg
+curl -L http://chocolatey.org/api/v2/package/nxlog/2.8.1248 -o nupkg/nxlog.2.8.1248.nupkg
+curl -L http://chocolatey.org/api/v2/package/nxlog/2.7.1191 -o nupkg/nxlog.2.7.1191.nupkg
+curl -L http://chocolatey.org/api/v2/package/nxlog/2.6.1131 -o nupkg/nxlog.2.6.1131.nupkg
+curl -L http://chocolatey.org/api/v2/package/nxlog/2.5.1089 -o nupkg/nxlog.2.5.1089nupkg
